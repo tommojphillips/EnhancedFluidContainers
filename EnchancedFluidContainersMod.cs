5d9d77f8fb1500958aeb9e700a317e898f2c5308
Tommo J. Phillips <tommojphillips@gmail.com>
2019-04-06 13:51:56 +1100

    Add project files.

---
+++ EnchancedFluidContainersMod.cs
@@ -0,0 +1,19 @@
+﻿using MSCLoader;
+
+namespace TommoJProductions.EnchancedFluidContainers
+{
+    public class EnchancedFluidContainersMod : Mod
+    {
+        public override string ID => "EnchancedFluidContainers";
+        public override string Name => "Enchaned Fluid Containers";
+        public override string Version => "0.1";
+        public override string Author => "tommojphillips";
+
+        public override void OnLoad()
+        {
+            // Written, 06.04.2019
+
+            ModConsole.Print(string.Format("{0} v{1}: Loaded", this.Name, this.Version));
+        }
+    }
+}
