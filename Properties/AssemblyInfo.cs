a6449f9c883ed6f799ad89c1a2a7d88d942e0b9a
Tommo J. Phillips <tommojphillips@gmail.com>
2022-06-09 18:04:36 +1000

    # Implemented auto versioning of mod. # Completely reworked how fluid containers are injected. # fixed null reference exceptions caused by container triggers arent in the same place depending on game save. # implemented twostroke trigger and particle offset.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,37 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General Information
 [assembly: AssemblyTitle("Enhanced Fluid Containers mod")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tommo J. Productions")]
 [assembly: AssemblyProduct("EnhancedFluidContainers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Tommo J. Productions")]
 [assembly: AssemblyCopyright("Copyright © Tommo J. Productions 2022")]
 [assembly: AssemblyTrademark("Azine")]
-[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-AU")]
+[assembly: AssemblyConfiguration("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+// Version information
+[assembly: AssemblyVersion("1.0.457.3")]
+//[assembly: AssemblyFileVersion("1.0.457.3")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b8e3794c-d4cb-487d-a121-1b30117d575c")]
+public class VersionInfo
+{
+	public const string lastestRelease = "03.04.2023 07:17 PM";
+	public const string version = "1.0.457.3";
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.5.0")]
-[assembly: AssemblyFileVersion("0.1.5.0")]
+    /// <summary>
+    /// Represents if the mod has been complied for x64
+    /// </summary>
+    #if x64
+        internal const bool IS_64_BIT = true;
+    #else
+        internal const bool IS_64_BIT = false;
+    #endif
+    #if DEBUG
+        internal const bool IS_DEBUG_CONFIG = true;
+    #else
+        internal const bool IS_DEBUG_CONFIG = false;
+    #endif
+}
+
